a2919d01fd35821c0339268a391ed5399a107a60
Martin Schijf <martinschijf@bol.com>
2024-09-20 20:06:57 +0200

    - adding deploy script to pom - storing scripts

---
+++ script/deploy.sh
@@ -0,0 +1,11 @@
+program=eneco-selenium
+path=eneco-selenium
+local_source_path=/Users/martinschijf/Prive/Sources/Kotlin/
+local_target_path=/Users/martinschijf/Prive/runeneco/
+
+cd $local_source_path/$path
+
+rm $local_target_path/$program*.jar
+cp target/$program*.jar $local_target_path
+
+
